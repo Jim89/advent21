5803d9bcf4b9c68984f5aea8aa87c88198d0a909
Jim Leach <jimmy22theave@gmail.com>
2021-12-01 12:15:13 -0500

    Answer in Python, too

---
+++ 1/answer.py
@@ -0,0 +1,11 @@
+# Make the input
+f = open("1/input.txt")
+numbers = [int(n) for n in f]
+f.close()
+
+# Answer 1
+sum([number > numbers[index-1] for index, number in enumerate(numbers) if index > 0])
+
+# Answer 2
+groups = [sum(numbers[index:index+3]) for index in range(len(numbers)) if index < len(numbers) - 2]
+sum([number > groups[index-1] for index, number in enumerate(groups) if index > 0])
