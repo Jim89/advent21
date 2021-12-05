7c3448039f97c6777aa1ffc7eaba7e7c9fbd3466
jim <jimmy22theave@gmail.com>
2021-12-05 10:34:25 -0500

    Solve Day 5 part 2, and tidy up results

--- 5/answer.R
+++ 5/answer.R
@@ -1,6 +1,14 @@
 library(purrr)
 library(stringr)
 
+# Functions for reading and building the base diagram --------------------------
+get_coords <- function(.input = "5/sample.txt") {
+    .input |>
+        readLines() |>
+        map(parse_coords) |>
+        {\(x) do.call(rbind, x)}()
+}
+
 parse_coords <- function(.line) {
     .line |>
         str_replace_all(" -> ", ",") |>
@@ -10,18 +18,32 @@ parse_coords <- function(.line) {
         matrix(nrow = 1, ncol = 4, dimnames = list("", c("x1", "y1", "x2", "y2")))
 }
 
-get_coords <- function(.input = "5/sample.txt") {
-    .input |>
-        readLines() |>
-        map(parse_coords) |>
-        {\(x) do.call(rbind, x)}()
-}
 
 build_diagram_base <- function(.mat) {
     # Because coords are 0-indexed and R is 1-indexed: add one
     x <- max(c(.mat[, "x1", drop = TRUE], .mat[, "x2", drop = TRUE])) + 1
     y <- max(c(.mat[, "y1", drop = TRUE], .mat[, "y2", drop = TRUE])) + 1
     matrix(0, nrow = y, ncol = x)
+}
+
+
+# Functions for doing the line drawing -----------------------------------------
+draw_lines <- function(.coords_mat, .base_diagram, with_diagonal = FALSE) {
+    .coords_mat |>
+        apply(1, draw_line, .base_diagram, with_diagonal, simplify = FALSE) |>
+        reduce(`+`)
+}
+
+draw_line <- function(.coords, base_diagram, with_diagonal = FALSE) {
+    if (is_horizontal(.coords)) {
+        draw_horizontal(.coords, base_diagram)
+    } else if (is_vertical(.coords)) {
+        draw_vertical(.coords, base_diagram)
+    } else if (with_diagonal) {
+        draw_diagonal(.coords, base_diagram)
+    } else {
+        base_diagram
+    }
 }
 
 is_horizontal <- function(.coords) {
@@ -50,28 +72,36 @@ draw_vertical <- function(.coords, base_diagram) {
     base_diagram
 }
 
-draw_line <- function(.coords, base_diagram) {
-    if (is_horizontal(.coords)) {
-        draw_horizontal(.coords, base_diagram)
-    } else if (is_vertical(.coords)) {
-        draw_vertical(.coords, base_diagram)
-    }
+draw_diagonal <- function(.diag_coords, base_diagram) {
+    diag_coords <- build_diagonal_line(.diag_coords)
+    adjusted_coords <- diag_coords + 1 # Because coords are 0-indexed and R is 1-indexed
+    base_diagram[adjusted_coords] <- 1
+    base_diagram
 }
 
-draw_lines <- function(.coords_mat, .base_diagram) {
-    .coords_mat |>
-        apply(1, draw_line, base_diagram = .base_diagram, simplify = FALSE) |>
-        compact() |>
-        reduce(`+`)
+build_diagonal_line <- function(.coords) {
+    xseq <- seq(.coords[["x1"]], .coords[["x2"]])
+    yseq <- seq(.coords[["y1"]], .coords[["y2"]])
+    # x is cols, y is rows in 'diagram', so for subsetting when drawing we want
+    # first col = rows, 2nd col = columns
+    cbind(yseq, xseq)
 }
 
+# Summary function to calcualte the result -------------------------------------
 overlaps <- function(.mat) {
     sum(.mat >= 2)
 }
 
-
+# Part 1 -----------------------------------------------------------------------
 coords_mat <- get_coords("5/input.txt")
 diagram <- build_diagram_base(coords_mat)
 all_lines <- draw_lines(coords_mat, diagram)
 overlaps(all_lines)
+
+# Part 2 -----------------------------------------------------------------------
+coords_mat <- get_coords("5/input.txt")
+diagram <- build_diagram_base(coords_mat)
+all_lines <- draw_lines(coords_mat, diagram, TRUE)
+overlaps(all_lines)
+
 
