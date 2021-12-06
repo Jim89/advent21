68d0e835b26016367cdc6544db94e165cbe6e191
jim <jimmy22theave@gmail.com>
2021-12-06 08:01:47 -0500

    Simplify to use fish frequencies, not fish
    Inspired by David Robinson, although he did something with matrix manips
    that I haven't 'got' yet!

--- 6/answer.R
+++ 6/answer.R
@@ -1,44 +1,20 @@
 starting_fish <- function(.input) scan(.input, what = integer(0), sep = ",")
 
-sim_day <- function(.fish) {
-    n_new <- sum(.fish == 0)
-    out <- rep(NA_integer_, length(.fish) + n_new)
-    f <- .fish - 1
-    f[which(f == -1)] <- 6
-    out[1:length(f)] <- f
-    out[is.na(out)] <- 8
-    out
-}
-
 sim_days <- function(.fish, days = 10) {
-    out <- list(rep(NA, days))
-    f <- .fish
+    all_freqs <- sapply(0:8, \(x) length(which(.fish == x)))
     for (i in seq_len(days)) {
-        out[[i]] <- sim_day(f)
-        f <- out[[i]]
+        n_new <- all_freqs[[1]]
+        all_freqs[1:8] <- all_freqs[2:9]
+        all_freqs[[7]] <- all_freqs[[7]] + n_new
+        all_freqs[[9]] <- n_new
     }
-    out
+    all_freqs
 }
 
-sim_days_out_only <- function(.fish, days = 10) {
-    f <- .fish
-    for (i in seq_len(days)) {
-        new <- sim_day(f)
-        f <- new
-    }
-    new
-}
 
 .days <- 80
-fish <- starting_fish("6/input.txt")
+fish <- starting_fish("6/sample.txt")
 sim_result <- sim_days(fish, .days)
-length(sim_result[[.days]])
+sum(sim_result)
 
-sim_result2 <- sim_days_out_only(fish, .days)
-length(sim_result2)
-
-results <- data.frame(x = 1:length(sim_result), y = sapply(sim_result, length))
-results$diff <- c(0, diff(results$y))
-plot(results$x, results$y)
-plot(results$x, results$diff)
 
