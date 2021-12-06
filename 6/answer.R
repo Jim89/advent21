5104fc3449b05535b2021ccba7e852bc61aa783b
jim <jimmy22theave@gmail.com>
2021-12-06 08:16:41 -0500

    Further cleanup

--- 6/answer.R
+++ 6/answer.R
@@ -1,21 +1,27 @@
 starting_fish <- function(.input) scan(.input, what = integer(0), sep = ",")
 
 sim_days <- function(.fish, days = 10) {
-    all_freqs <- as.double(sapply(0:8, \(x) length(which(.fish == x))))
+    # Count how many fish of each age there are, from 0 -> 8
+    fish_ages <- as.double(sapply(0:8, \(x) length(which(.fish == x))))
     for (i in seq_len(days)) {
-        n_new <- all_freqs[[1]]
-        all_freqs[1:8] <- all_freqs[2:9]
-        all_freqs[[7]] <- all_freqs[[7]] + n_new
-        all_freqs[[9]] <- n_new
+        n_new_fish <- fish_ages[[1]]
+        fish_ages[1:8] <- fish_ages[2:9] # Age all fish by 1
+        fish_ages[[7]] <- fish_ages[[7]] + n_new_fish # Add the new fish at age 6
+        fish_ages[[9]] <- n_new_fish # "Cycle" the fish that just gave birth
     }
-    all_freqs
+    fish_ages
 }
 
 
 fish <- starting_fish("6/input.txt")
-.days <- 80
-sim_result <- sim_days(fish, .days)
+
+# Part 1
+fish |>
+    sim_days(80) |>
+    sum()
+
+# Part 2
 options(scipen = 999)
-sum(sim_result, na.rm = TRUE)
-
-
+fish |>
+    sim_days(256) |>
+    sum()
