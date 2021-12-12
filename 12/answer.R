cf6cdf45e3e6d1d364c85b2a4e2674c2d10cf874
jim <jimmy22theave@gmail.com>
2021-12-12 09:48:34 -0500

    Work on part 1 solution for day 12
    Nearly there, but need to explore all neighbours as starting point
    (BFS), not just DFS until we find a path

---
+++ 12/answer.R
@@ -0,0 +1,69 @@
+library(igraph)
+read_edgelist <- function(.path) {
+    readLines("12/smallest.txt") |>
+        strsplit("-") |>
+        lapply(rbind) |>
+        (\(x) do.call(rbind, x))()
+}
+
+build_adjaceny_mat <- function(.el, directed = FALSE) {
+    .el |>
+        graph_from_edgelist(directed) |>
+        as_adjacency_matrix() |>
+        as.matrix()
+}
+
+is_lower <- function(.letters) all(strsplit(.letters, "")[[1]] %in% letters)
+is_upper <- function(.letters) all(strsplit(.letters, "")[[1]] %in% LETTERS)
+
+sel <- read_edgelist("12/smallest.txt")
+am <- build_adjaceny_mat(sel)
+
+
+test <- find_all_paths(g, "start", "end")
+
+# 1. From source, visit neighbours
+# 1. Track those visited
+# 1. For each neighbour, make that source and repeat
+    # 1. But don't _revist_ lowercase neighbours
+# 1. If neighbour == end, stop, found a path
+
+find_paths <- function(adj_mat, src = "start", tgt = "end") {
+    visited <- NULL
+}
+
+neighbours <- function(.am, .n) {
+    row <- .am[.n, ]
+    names(which(row == 1))
+}
+have_visited <- function(node, .visited) node %in% .visited
+
+
+# If we haven't visited it, do so
+# Otherwise, is it lowercase? If it is, stop, if it isn't, can visit again
+find_paths <- function(.graph, .src = "start", .tgt = "end", .path = vector(mode = "character")) {
+    if (.tgt %in% .path) {
+        return(.path)
+    } else {
+        # Visit the source node ("start" to begin with)
+        .path <- append(.path, .src)
+    }
+
+    if (.src == .tgt) return(.path)
+
+
+    # Find the neighbours
+    .neighbours <- neighbours(.graph, .src)
+
+    for (.neighbour in .neighbours) {
+        if (is_lower(.neighbour) & have_visited(.neighbour, .path)) {
+            next
+        } else {
+            .path <- find_paths(.graph, .neighbour, .tgt, .path)
+        }
+    }
+
+    return(.path)
+}
+
+find_paths(am, "start", "end")
