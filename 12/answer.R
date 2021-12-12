06619ceec8b8f650e2451a9714298af4781b54e2
jim <jimmy22theave@gmail.com>
2021-12-12 11:59:22 -0500

    Solve day 12, part 2

--- 12/answer.R
+++ 12/answer.R
@@ -1,6 +1,6 @@
 library(igraph)
 read_edgelist <- function(.path) {
-    readLines("12/smallest.txt") |>
+    readLines(.path) |>
         strsplit("-") |>
         lapply(rbind) |>
         (\(x) do.call(rbind, x))()
@@ -13,40 +13,31 @@ build_adjaceny_mat <- function(.el, directed = FALSE) {
         as.matrix()
 }
 
-is_lower <- function(.letters) all(strsplit(.letters, "")[[1]] %in% letters)
 is_upper <- function(.letters) all(strsplit(.letters, "")[[1]] %in% LETTERS)
+is_lower <- function(.letters) !is_upper(.letters)
 
-sel <- read_edgelist("12/smallest.txt")
-am <- build_adjaceny_mat(sel)
-
-
-test <- find_all_paths(g, "start", "end")
-
-# 1. From source, visit neighbours
-# 1. Track those visited
-# 1. For each neighbour, make that source and repeat
-    # 1. But don't _revist_ lowercase neighbours
-# 1. If neighbour == end, stop, found a path
-
-find_paths <- function(adj_mat, src = "start", tgt = "end") {
-    visited <- NULL
-}
 
 neighbours <- function(.am, .n) {
     row <- .am[.n, ]
     names(which(row == 1))
 }
-have_visited <- function(node, .visited) node %in% .visited
-
-
-# If we haven't visited it, do so
-# Otherwise, is it lowercase? If it is, stop, if it isn't, can visit again
-find_paths <- function(.graph, .src = "start", .tgt = "end", .path = vector(mode = "character")) {
-    if (.tgt %in% .path) {
-    } else {
-        # Visit the source node ("start" to begin with)
-        .path <- append(.path, .src)
+have_visited <- function(node, .visited, part2 = FALSE) {
+    if (node == "start" | node == "end") {
+        return(node %in% .visited)
     }
+    if (part2) {
+        if (is_upper(node)) {
+            FALSE
+        } else {
+            sum(node == .visited) >= 2
+        }
+    } else {
+        node %in% .visited
+    }
+}
+
+find_paths <- function(.graph, .src = "start", .tgt = "end", .path = vector(mode = "character"), part2 = FALSE) {
+    .path <- append(.path, .src)
 
     if (.src == .tgt) return(.path)
 
@@ -56,13 +47,55 @@ find_paths <- function(.graph, .src = "start", .tgt = "end", .path = vector(mode
     .neighbours <- neighbours(.graph, .src)
 
     for (.neighbour in .neighbours) {
-        if (!have_visited(.neighbour, .path) | is_upper(.neighbour) ) {
-            .other_paths <- find_paths(.graph, .neighbour, .tgt, .path)
-            .all_paths <- append(.all_paths, list(.other_paths))
+        if (part2) {
+            .visits <- table(.path[sapply(.path, is_lower)])
+            .visited_twice <- .visits > 1
+            if (any(.visited_twice)) {
+                if (!have_visited(.neighbour, .path) | is_upper(.neighbour) ) {
+                    .other_paths <- find_paths(.graph, .neighbour, .tgt, .path)
+                    .all_paths <- append(.all_paths, .other_paths)
+                }
+            } else {
+                if (!have_visited(.neighbour, .path, part2 = TRUE) | is_upper(.neighbour) ) {
+                    .other_paths <- find_paths(.graph, .neighbour, .tgt, .path, part2 = TRUE)
+                    .all_paths <- append(.all_paths, .other_paths)
+                }
+
+            }
+        } else {
+            if (!have_visited(.neighbour, .path) | is_upper(.neighbour) ) {
+                .other_paths <- find_paths(.graph, .neighbour, .tgt, .path)
+                .all_paths <- append(.all_paths, .other_paths)
+            }
         }
     }
-
+    # unlist() all paths?
     return(.all_paths)
 }
 
-out <- find_paths(am, "start", "end")
+
+
+n_paths <- function(.input, part2 = FALSE) {
+    el <- read_edgelist(.input)
+    am <- build_adjaceny_mat(el)
+    out <- find_paths(am, "start", "end", part2 = part2)
+    paths <- purrr::map2(
+        which(out == "start"),
+        which(out == "end"),
+        \(x, y) unlist(out[x:y])
+    )
+    length(paths)
+}
+
+n_paths("12/smallest.txt") # Part 1: 10
+n_paths("12/larger.txt") # Part 1: 19
+n_paths("12/largest.txt") # Part 1: 226
+n_paths("12/input.txt") # Part 1: 3450
+
+
+n_paths("12/smallest.txt", TRUE) # Part 2: 36
+n_paths("12/larger.txt", TRUE) # Part 2: 103
+n_paths("12/largest.txt", TRUE) # Part 2: 3509
+n_paths("12/input.txt", TRUE) # Part 2: 96528
+
+
