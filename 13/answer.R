120e3c922b102a703b5b6dd87f89ad7f33dcd52e
jim <jimmy22theave@gmail.com>
2021-12-13 08:34:53 -0500

    Add ggplot2 visualisation, too

--- 13/answer.R
+++ 13/answer.R
@@ -37,8 +37,17 @@ fold_up <- function(.matrix, .row) {
 
     folded_bottom_half <- .matrix[nrow(.matrix):(.row+1), , drop = FALSE]
 
-    top_half | folded_bottom_half
+    if (nrow(top_half) != nrow(folded_bottom_half)) {
+        filler <- matrix(FALSE, abs(nrow(top_half) - nrow(folded_bottom_half)), ncol = ncol(top_half))
 
+        if (nrow(top_half) < nrow(folded_bottom_half)) {
+            top_half <- rbind(filler, top_half)
+        } else {
+            folded_bottom_half <- rbind(filler, folded_bottom_half)
+        }
+    }
+
+    top_half | folded_bottom_half
 }
 
 fold_left <- function(.matrix, .col) {
@@ -74,12 +83,42 @@ sample_mat <- read_matrix("13/sample.txt", 18, .split = ",")
 sample_folds <- read_folds("13/sample.txt")
 
 sample_mat |>
-    fold(sample_folds[[1]]) |>
+    fold(sample_folds[[1]]) |> sum()
     fold(sample_folds[[2]])
 
 
 mat <- read_matrix("13/input.txt", n = 866, .split = ",")
 folds <- read_folds("13/input.txt")
+
+# Part 1
 mat |>
     fold(folds[[1]]) |>
     sum()
+
+# Iterate over folder for part 2
+all_folds <- function(.mat, .folds) {
+    mat <- .mat
+    for (i in seq_along(.folds)) {
+        new_mat <- fold(mat, .folds[[i]])
+        mat <- new_mat
+    }
+    mat
+}
+
+# Cheating solution to "draw" the letters!
+all_folds(mat, folds) |>
+    raster() |>
+    plot()
+
+
+# With ggplot2
+all_folds(mat, folds) |>
+    as.data.frame() |>
+    tibble::as_tibble() |>
+    dplyr::mutate(rn = dplyr::row_number()) |>
+    tidyr::pivot_longer(-rn) |>
+    dplyr::mutate(rn = -1*rn, name = readr::parse_number(name)) |>
+    ggplot2::ggplot(ggplot2::aes(name, rn, fill = value)) +
+    ggplot2::geom_tile() +
+    ggplot2::scale_fill_manual(values = c("TRUE" = "black", "FALSE" = "white"), guide = "none") +
+    ggplot2::theme_void()
