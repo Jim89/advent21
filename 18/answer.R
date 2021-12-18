16bc72933542a2b7afee89588ff8b3c999e9b605
jim <jimmy22theave@gmail.com>
2021-12-18 13:14:34 -0500

    Fix exploding and prevent stack overflow
    Because we can explode _after_ exploding, and hence have some of the
    number be more than 2 digits, my previous approach of assuming a fixed
    pair length didn't work, so this change fixes that, allowing for (e.g.)
    both [1, 2] and [11, 2] or [11, 10] (i.e. double-digits inside the
    pair).
    
    I was also running into cstack usage/recursion limits which were
    actually caused by paste0(, collapse = ""), rather than _my_ recursion
    (or maybe in addition to my recursion), so instead used regexes to do
    find-and-replacement in the string (rather than strsplit/paste0)

--- 18/answer.R
+++ 18/answer.R
@@ -17,47 +17,42 @@ explode_locations <- function(sf_number) {
     pair_starts[depths_along_number[pair_starts] > depth]
 }
 
-explode <- function(sf_number, pair_location, debug = F) {
+explode_sf <- function(sf_number, pair_location, debug = F) {
     if (debug) browser()
 
     left_of_pair <- substr(sf_number, 1, pair_location - 1)
-    right_of_pair <- substr(sf_number, pair_location + 5, nchar(sf_number))
 
-    pair <- substr(sf_number, pair_location, pair_location + 4)
+    pair_end <- min(stringr::str_locate(substr(sf_number, pair_location, nchar(sf_number)), "\\]"))
+    pair <- substr(sf_number, pair_location, pair_location + pair_end-1)
     pair_numbers <- stringr::str_extract_all(pair, "\\d+")[[1]]
     pair_left <- as.numeric(pair_numbers[[1]])
     pair_right <- as.numeric(pair_numbers[[2]])
 
+    right_of_pair_start <- pair_location + pair_end
+    right_of_pair <- substr(sf_number, right_of_pair_start, nchar(sf_number))
 
     number_to_left <- number_to_right <- 0
     left_updated <- left_of_pair
     if (grepl("\\d+", left_of_pair)) {
-        left_split <- strsplit(left_of_pair, "")[[1]]
+        # Find the last digit in the LHS of the pair
+        number_to_left <- as.numeric(stringr::str_extract(left_of_pair, "(\\d+)(?!.*\\d)"))
 
-        left_split_rev <- rev(left_split)
-        left_split_rev_numbers <- left_split_rev[left_split_rev %in% 0:9]
-
-        number_to_left <- as.numeric(left_split_rev_numbers[[1]])
         replacement_left <- number_to_left + pair_left
 
-        left_split[max(which(left_split %in% 0:9))] <- replacement_left
-        left_updated <- paste0(left_split, collapse = "")
-
+        # Perform the replacement
+        left_updated <- stringr::str_replace(left_of_pair, "(\\d+)(?!.*\\d)", as.character(replacement_left))
     }
 
     right_updated <- right_of_pair
     if (grepl("\\d+", right_of_pair)) {
-        right_split <- strsplit(right_of_pair, "")[[1]]
+        # Find the first digit
+        number_to_right <- as.numeric(stringr::str_extract(right_of_pair, "\\d{1,2}"))
 
-        right_split_numbers <- right_split[right_split %in% 0:9]
-
-        number_to_right <- as.numeric(right_split_numbers[[1]])
         replacement_right <- number_to_right + pair_right
 
-        right_split[min(which(right_split %in% 0:9))] <- replacement_right
-        right_updated <- paste0(right_split, collapse = "")
+        # Perform the replacement
+        right_updated <- stringr::str_replace(right_of_pair, "\\d{1,2}", as.character(replacement_right))
     }
-
     new <- paste0(left_updated, "0", right_updated)
     if (stringr::str_detect(new, "\\[\\d{1,2}\\]")) browser()
     reduce_once(new)
